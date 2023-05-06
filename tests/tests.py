488ace44210a85a63bd91a1d100732aed2e19665
Natanim <120901757+natanimn@users.noreply.github.com>
2023-05-06 13:04:14 +0300

    Add files via upload

---
+++ tests/tests.py
@@ -0,0 +1,33 @@
+from filters import parse_time
+import unittest
+import time
+from model import User
+from app import db, create_app
+
+
+class TestDb(unittest.TestCase):
+
+    def setUp(self) -> None:
+        app = create_app("test")
+        self.db = db
+        with app.app_context() as self.context:
+            self.context.push()
+
+    def test_user(self):
+        self.assertIsNone(User.query.filter_by(id=1).first())
+        self.assertEqual([], User.query.all())
+
+    def tearDown(self) -> None:
+        self.context.pop()
+
+
+class TestTime(unittest.TestCase):
+    def test_time(self):
+        self.assertEqual("አሁን", parse_time(time.time()))
+        self.assertEqual("ከ30 ሰከንድ በፊት", parse_time(time.time() - 30))
+        self.assertEqual("ከ2 ደቂቃዎች በፊት", parse_time(time.time() - 120))
+        self.assertEqual("ከ2 ሰዓታት በፊት", parse_time(time.time() - 2*3600))
+
+
+if __name__ == '__main__':
+    unittest.main()
