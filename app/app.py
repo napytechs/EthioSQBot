5281184b71a1396f8d6124c205d32e0a3d619068
Natanim <120901757+natanimn@users.noreply.github.com>
2023-05-06 13:02:04 +0300

    Add files via upload

---
+++ app/app.py
@@ -0,0 +1,16 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from . import config
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+
+def create_app(config_name):
+    from flask import Flask
+    app = Flask(__name__)
+    app.config.from_object(config.config[config_name])
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    return app
