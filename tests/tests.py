c1d85901068b6c1a2c9aef4f14ff46cf4cd1ec67
Natanim <120901757+natanimn@users.noreply.github.com>
2023-06-01 07:29:35 +0300

    አዲስ ሙከራ ተጨምሯል።

--- tests/tests.py
+++ tests/tests.py
@@ -1,24 +1,32 @@
-from utils.filters import parse_time
+from utils.filters import parse_time, smart_subject as smart_hash_subject
 import unittest
 import time
-from utils.model import User
-from app import db, create_app
 
 
-class TestDb(unittest.TestCase):
-
-    def setUp(self) -> None:
-        app = create_app("test")
-        self.db = db
-        with app.app_context() as self.context:
-            self.context.push()
-
-    def test_user(self):
-        self.assertIsNone(User.query.filter_by(id=1).first())
-        self.assertEqual([], User.query.all())
-
-    def tearDown(self) -> None:
-        self.context.pop()
+class CustomTestCase(unittest.TestCase):
+    def test_each_subject(self):
+        self.assertEqual("#አማርኛ", smart_hash_subject("🇪🇹 አማርኛ"))
+        self.assertEqual("#አፋን_ኦሮሞ", smart_hash_subject("🇪🇹 አፋን ኦሮሞ"))
+        self.assertEqual("#ኢንግሊሽ", smart_hash_subject("🇬🇧 ኢንግሊሽ"))
+        self.assertEqual("#ኬሚስትሪ", smart_hash_subject("🧪 ኬሚስትሪ"))
+        self.assertEqual("#ሒሳብ", smart_hash_subject("🧮 ሒሳብ"))
+        self.assertEqual("#ፊዚክስ", smart_hash_subject("🔭 ፊዚክስ"))
+        self.assertEqual("#ስነህይወት", smart_hash_subject("🔬 ስነህይወት|Biology"))
+        self.assertEqual("#ስነህይወት", smart_hash_subject("🔬 ስነህይወት | Biology"))
+        self.assertEqual("#ስነህይወት", smart_hash_subject("🔬 ስነህይወት"))
+        self.assertEqual("#ታሪክ", smart_hash_subject("🌏 ታሪክ"))
+        self.assertEqual("#ጂኦግራፊ", smart_hash_subject("🧭 ጂኦግራፊ"))
+        self.assertEqual("#ስነዜጋ", smart_hash_subject("⚖ ስነዜጋ|Civics"))
+        self.assertEqual("#ስነዜጋ", smart_hash_subject("⚖ ስነዜጋ | Civics"))
+        self.assertEqual("#ስነዜጋ", smart_hash_subject("⚖ ስነዜጋ"))
+        self.assertEqual("#ስነብዕል", smart_hash_subject("💶 ስነብዕል|Economics"))
+        self.assertEqual("#ስነብዕል", smart_hash_subject("💶 ስነብዕል | Economics"))
+        self.assertEqual("#ስነብዕል", smart_hash_subject("💶 ስነብዕል"))
+        self.assertEqual("#ቢዝነስ", smart_hash_subject("💰 ቢዝነስ"))
+        self.assertEqual("#ህብረተስብ", smart_hash_subject("👥 ህብረተስብ|Social"))
+        self.assertEqual("#ህብረተስብ", smart_hash_subject("👥 ህብረተስብ | Social"))
+        self.assertEqual("#ህብረተስብ", smart_hash_subject("👥 ህብረተስብ"))
+        self.assertEqual("#ጠቅላላ_እውቀት", smart_hash_subject("🧠 ጠቅላላ እውቀት"))
 
 
 class TestTime(unittest.TestCase):
