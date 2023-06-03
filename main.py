4a60bcba3ad696a02efca6b60e1f17f48a0901dd
Natanim <120901757+natanimn@users.noreply.github.com>
2023-06-03 12:24:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ import logging
 
 logger = logging.getLogger("TeleBot")
 logger.setLevel(logging.INFO)
+
 apihelper.ENABLE_MIDDLEWARE = True
 TOKEN = os.getenv("TOKEN")
 bot = TeleBot(TOKEN, parse_mode='html')
